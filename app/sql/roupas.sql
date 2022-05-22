8d0a8cd9d199da6aed154af3c8e5499ef2f877d9
Lessa Apolinario <lessaapolinario@gmail.com>
2022-05-21 21:10:17 -0300

    Add database with the initial tables

---
+++ app/sql/roupas.sql
@@ -0,0 +1,30 @@
+CREATE
+DATABASE loja;
+
+USE
+loja;
+
+CREATE TABLE categoria
+(
+    id PRIMARY KEY AUTO_INCREMENT,
+    nome VARACHAR(200)
+);
+
+CREATE TABLE roupa
+(
+    id           INT PRIMARY KEY AUTO_INCREMENT,
+    nome         VARCHAR(50),
+    preco        FLOAT,
+    descricao    VARCHAR(200),
+    numero       INT,
+    categoria_id INT
+        CONSTRAINT rp_cat_id FOREIGN KEY (categoria_id) REFERENCES categoria(id);
+);
+
+CREATE TABLE cliente
+(
+    id PRIMARY KEY AUTO_INCREMENT,
+    nome    VARCHAR(200),
+    usuario VARCHAR(200),
+    senha   VARCHAR(200)
+);
