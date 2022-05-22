d588686087da77b353485b16fd06650bbab258c9
Lessa Apolinario <lessaapolinario@gmail.com>
2022-05-22 16:01:18 -0300

    Add create method for RoupaDAO

---
+++ app/models/RoupaDAO.php
@@ -0,0 +1,28 @@
+<?php
+require_once "./database/MySQLDatabase.php";
+require_once "./DAO.php";
+
+class RoupaDAO extends DAO
+{
+    private $database;
+    private $tableName = "roupa";
+
+    public function __construct()
+    {
+        $this->database = new MySQLDatabase();
+        parent::__construct($this->database);
+        parent::setTableName($this->tableName);
+    }
+
+    public function create($roupa) {
+        $values = "nome, preco, descricao, numero, quantidade";
+        $data = ":nome, :preco, :descricao, :numero, :quantidade";
+        $stmt = $this->database->insert($values, $data);
+        $stmt->bindValue(":nome", $roupa->getNome());
+        $stmt->bindValue(":preco", $roupa->getPreco());
+        $stmt->bindValue(":descricao", $roupa->getDescricao());
+        $stmt->bindValue(":numero", $roupa->getNumero());
+        $stmt->bindValue(":quantidade", $roupa->getQuantidade());
+        return $stmt->execute();
+    }
+}
