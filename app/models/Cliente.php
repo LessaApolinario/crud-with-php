9770810c7676fd03b02ae7bd28ad808a7e16196e
Lessa Apolinario <lessaapolinario@gmail.com>
2022-05-22 15:27:23 -0300

    Add Cliente class fields and methods

--- app/models/Cliente.php
+++ app/models/Cliente.php
@@ -2,5 +2,56 @@
 
 class Cliente
 {
+    private $id;
+    private $nome;
+    private $usuario;
+    private $senha;
 
+    public function __construct($id, $nome, $usuario, $senha)
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->usuario = $usuario;
+        $this->senha = $senha;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+    public function setUsuario($usuario): void
+    {
+        $this->usuario = $usuario;
+    }
+
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    public function setSenha($senha): void
+    {
+        $this->senha = $senha;
+    }
 }
