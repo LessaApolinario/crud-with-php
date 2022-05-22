1cc6ef93108ae188bfa8753529d94edaf9958b0f
Lessa Apolinario <lessaapolinario@gmail.com>
2022-05-22 15:24:46 -0300

    Add Roupa class fields and methods

--- app/models/Roupa.php
+++ app/models/Roupa.php
@@ -2,5 +2,80 @@
 
 class Roupa
 {
+    private $id;
+    private $nome;
+    private $preco;
+    private $descricao;
+    private $numero;
+    private $quantidade;
 
+    public function __construct($id, $nome, $preco, $descricao, $numero, $quantidade)
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->preco = $preco;
+        $this->descricao = $descricao;
+        $this->numero = $numero;
+        $this->quantidade = $quantidade;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    public function setPreco($preco): void
+    {
+        $this->preco = $preco;
+    }
+
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao): void
+    {
+        $this->descricao = $descricao;
+    }
+
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    public function setNumero($numero): void
+    {
+        $this->numero = $numero;
+    }
+
+    public function getQuantidade()
+    {
+        return $this->quantidade;
+    }
+
+    public function setQuantidade($quantidade): void
+    {
+        $this->quantidade = $quantidade;
+    }
 }
