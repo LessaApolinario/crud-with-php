5863919a7f4062ff59633e14279fb0406624de2e
Lessa Apolinario <lessaapolinario@gmail.com>
2022-05-22 22:07:32 -0300

    Add cadastrar view for roupa

---
+++ app/views/roupa/cadastrar.php
@@ -0,0 +1,28 @@
+<form action="../../index.php?classe=Roupa&acao=cadastrar" method="POST">
+    <p>
+        <label>Nome:</label>
+        <input type="text" name="nome"/>
+    </p>
+
+    <p>
+        <label>Preço:</label>
+        <input type="text" name="preco"/>
+    </p>
+
+    <p>
+        <label>Descrição:</label>
+        <input type="text" name="descricao"/>
+    </p>
+
+    <p>
+        <label>Número:</label>
+        <input type="text" name="numero"/>
+    </p>
+
+    <p>
+        <label>Quantidade:</label>
+        <input type="text" name="quantidade"/>
+    </p>
+
+    <button>Cadastrar</button>
+</form>
