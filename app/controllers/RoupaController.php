e7cd811c41e207521639812a24df301bb8d2dfdd
Lessa Apolinario <lessaapolinario@gmail.com>
2022-05-22 21:42:23 -0300

    Add cadastrar method for RoupaController

--- app/controllers/RoupaController.php
+++ app/controllers/RoupaController.php
@@ -1,6 +1,24 @@
 <?php
+require_once "../models/Roupa.php";
+require_once "../models/RoupaDAO.php";
 
 class RoupaController
 {
+    private $roupa;
+    private $roupaDAO;
 
+    public function cadastrar()
+    {
+        $nome = $_REQUEST["nome"];
+        $preco = $_REQUEST["preco"];
+        $descricao = $_REQUEST["descricao"];
+        $numero = $_REQUEST["numero"];
+        $quantidade = $_REQUEST["quantidade"];
+        $this->roupa = new Roupa($nome, $preco, $descricao, $numero, $quantidade);
+        $this->roupaDAO = new RoupaDAO();
+
+        if ($this->roupaDAO->cadastrar($this->roupa)) {
+            require_once "./views/roupa/cadastrar.php";
+        }
+    }
 }
