1eae5ce884d269747b9ebdeac0669c7cb6aa1e27
Lessa Apolinario <lessaapolinario@gmail.com>
2022-05-28 20:34:03 -0300

    Fix some PDO arguments syntax

--- app/models/database/MySQLDatabase.php
+++ app/models/database/MySQLDatabase.php
@@ -25,7 +25,7 @@ class MySQLDatabase implements IDatabase
 
     public function connect()
     {
-        $this->connection = new PDO("mysql:host:$this->host;dbname:$this->dbname", $this->username, $this->password);
+        $this->connection = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
         $this->connection->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
     }
 
