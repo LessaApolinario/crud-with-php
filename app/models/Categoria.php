93de56df3ea3a3b2511744aa4b118936fc4393c2
Lessa Apolinario <lessaapolinario@gmail.com>
2022-05-30 15:01:06 -0300

    Remove Categoria class constructor

--- app/models/Categoria.php
+++ app/models/Categoria.php
@@ -5,12 +5,6 @@ class Categoria
     private $id;
     private $nome;
 
-    public function __construct($id, $nome)
-    {
-        $this->id = $id;
-        $this->nome = $nome;
-    }
-
     public function getId()
     {
         return $this->id;
