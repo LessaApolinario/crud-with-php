5d4493481d2d2137a5b964fafec8c46743417a91
Lessa Apolinario <lessaapolinario@gmail.com>
2022-05-30 14:33:01 -0300

    Add atualizar view

---
+++ app/views/roupa/atualizar.php
@@ -0,0 +1,33 @@
+<form action="../../index.php?classe=Roupa&acao=atualizar" method="POST">
+    <p>
+        <label>id:</label>
+        <input type="text" name="id"/>
+    </p>
+
+    <p>
+        <label>Nome:</label>
+        <input type="text" name="nome"/>
+    </p>
+
+    <p>
+        <label>Preço:</label>
+        <input type="text" name="preco"/>
+    </p>
+
+    <p>
+        <label>Descrição:</label>
+        <input type="text" name="descricao"/>
+    </p>
+
+    <p>
+        <label>Número:</label>
+        <input type="text" name="numero"/>
+    </p>
+
+    <p>
+        <label>Quantidade:</label>
+        <input type="text" name="quantidade"/>
+    </p>
+
+    <button>Atualizar</button>
+</form>
